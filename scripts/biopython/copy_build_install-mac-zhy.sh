128c3de7ea6cbbbe5bcc16a7771fcb22961e7a35
zhenghaiyong <zhenghaiyong@gmail.com>
2013-12-30 21:35:03 +0800

    Upgrade biopython from 1.62 to 1.63 using pip

--- scripts/biopython/copy_build_install-mac-zhy.sh
+++
@@ -1,3 +0,0 @@
-cp ~/workspace/GitHub/zhenghaiyong/BioInfo_CAHEC-VRL-ISM/scripts/biopython/BioSQL/Loader.py /Users/flyzhy/Desktop/CAHEC/zhy/biopython-1.62/BioSQL/
-python setup.py build
-sudo python setup.py install --record log_install
